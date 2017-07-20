fc8338cb4474acf9f88cda944c5a815a2c55fead
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-07-20 22:38:12 +0100

    wip

--- scripts/setup-test-webserver.sh
+++ scripts/setup-test-webserver.sh
@@ -5,14 +5,17 @@ echo "####### About to run setup-test-webserver.sh #######"
 echo "####################################################"
 
 yum install -y epel-release || exit 1
-yum group install -y 'gnome desktop' || exit 1
-yum install -y httpd || exit 1
-systemctl enable httpd || exit 1
-systemctl start httpd || exit 1
+# the following is required for autocomplete nmcli commands
+yum -y install bash-completion || exit 1
 
-systemctl isolate graphical.target & 
+#yum group install -y 'gnome desktop' || exit 1
+#yum install -y httpd || exit 1
+#systemctl enable httpd || exit 1
+#systemctl start httpd || exit 1
 
-systemctl set-default graphical.target || exit 1
+#systemctl isolate graphical.target & 
+
+#systemctl set-default graphical.target || exit 1
 
 
 exit 0
