0ba41a544d22b8e5fd57b9be80f6003f2bebb8df
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-03-31 19:41:18 +0100

    this is now working

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "bento/centos-7.4"
+  config.vm.box = "bento/centos-7.3"
 
   config.vm.provision "shell", path: "scripts/install-rpms.sh", privileged: true
   config.vm.provision "shell", path: "scripts/setup-network-teaming.sh", privileged: true
--- scripts/setup-network-teaming.sh
+++ scripts/setup-network-teaming.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 echo "####################################################"
 echo "####### About to run setup-network-teaming.sh #######"
 echo "####################################################"
@@ -8,20 +10,22 @@ echo "####################################################"
 yum install -y teamd
 
 nmcli connection add type team con-name CodingBeeTeam0 ifname CodingBeeTeam0 config '{"runner": {"name":"activebackup"}}'
-
+sleep 5
 teamdctl CodingBeeTeam0 state
 ip addr show
 
-
 nmcli connection modify CodingBeeTeam0 ipv4.addresses '192.168.2.50/24'
+nmcli connection modify CodingBeeTeam0 ipv4.method manual
 
 nmcli connection add type team-slave con-name CodingBeeTeam0-port1 ifname enp0s8 master CodingBeeTeam0
 nmcli connection add type team-slave con-name CodingBeeTeam0-port2 ifname enp0s9 master CodingBeeTeam0
-
 
 nmcli connection up CodingBeeTeam0
 nmcli connection up CodingBeeTeam0-port1
 nmcli connection up CodingBeeTeam0-port2
 
+
+teamdctl CodingBeeTeam0 state
+ip addr show
 
 exit 0
